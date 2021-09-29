1b9f2799112bb3d61abe8b7a8aadcffd35e4262c
debroucker <debroucker.tommy1gmail.com>
2021-09-29 19:06:48 +0200

    bug fix groupe anglais

--- src/cal.py
+++ src/cal.py
@@ -5,9 +5,10 @@ def get_all_dates(month, grp_english):
     url = 'https://calendar.google.com/calendar/ical/vkcaks7nqiiub3gp6a24po6gfk%40group.calendar.google.com/public/basic.ics'
     c = Calendar(requests.get(url).text)
     all_events = []
-    for event in c.events:  # le bon mois
-        if event.begin.month == month:  # groupe anglais
-            if 'Ang' in event.name:
+    print(grp_english)
+    for event in c.events:
+        if event.begin.month == month: # le bon mois
+            if 'Ang' in event.name:  # groupe anglais
                 if '(grp ' + grp_english in event.name:
                     all_events.append(event)
             elif 'seulement FI' not in event.name and 'Férié' not in event.name and 'Interruption pédagogique' not in event.name:  # osef des FI & jour fériés
