d161f382e2a9fa0eef6890fc544f320a37f3141e
debroucker <debroucker.tommy1gmail.com>
2021-09-29 19:23:05 +0200

    fix bug 1er cours anglais

--- src/cal.py
+++ src/cal.py
@@ -5,11 +5,14 @@ def get_all_dates(month, grp_english):
     url = 'https://calendar.google.com/calendar/ical/vkcaks7nqiiub3gp6a24po6gfk%40group.calendar.google.com/public/basic.ics'
     c = Calendar(requests.get(url).text)
     all_events = []
-    print(grp_english)
     for event in c.events:
         if event.begin.month == month: # le bon mois
             if 'Ang' in event.name:  # groupe anglais
-                if '(grp ' + grp_english in event.name:
+                added = False
+                if not ('(' in event.name) : #1er cours anglais
+                    all_events.append(event)
+                    added = True
+                if not added and '(grp ' + grp_english in event.name:
                     all_events.append(event)
             elif 'seulement FI' not in event.name and 'Férié' not in event.name and 'Interruption pédagogique' not in event.name:  # osef des FI & jour fériés
                 all_events.append(event)
